b880725d8642cb5924fc6bc85980fc522895e11a
yshalsager <ysh-alsager@hotmail.com>
2020-10-20 13:42:25 +0200

    adjust_layout_direction: handle statusBar dynamically
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- xiaomi_flashable_firmware_creator_gui/helpers/layout.py
+++ xiaomi_flashable_firmware_creator_gui/helpers/layout.py
@@ -2,14 +2,16 @@
 from PyQt5.QtCore import Qt
 
 
-def adjust_layout_direction(self, lang: str):
+def adjust_layout_direction(window, lang: str):
     """
     Change Layout Direction based on languages
     """
     rtl_languages = ['ar', 'az', 'dv', 'fa', 'he', 'ur']
     if lang in rtl_languages:
-        self.setLayoutDirection(Qt.RightToLeft)
-        self.statusBar().setLayoutDirection(Qt.RightToLeft)
+        window.setLayoutDirection(Qt.RightToLeft)
+        if hasattr(window, 'statusBar'):
+            window.statusBar().setLayoutDirection(Qt.RightToLeft)
     else:
-        self.setLayoutDirection(Qt.LeftToRight)
-        self.statusBar().setLayoutDirection(Qt.LeftToRight)
+        window.setLayoutDirection(Qt.LeftToRight)
+        if hasattr(window, 'statusBar'):
+            window.statusBar().setLayoutDirection(Qt.LeftToRight)
